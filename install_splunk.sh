f49330d55699f819ea575d1816f200b9da650027
bhanu4955 <65145092+bhanu4955@users.noreply.github.com>
2024-02-19 20:53:12 +0530

    Update install_splunk.sh

--- install_splunk.sh
+++ install_splunk.sh
@@ -6,16 +6,16 @@ INSTALL_DIR="/opt/splunk"
 #SPLUNK_PASSWORD="bhargavi23"
 
 # Create the installation directory
-sudo mkdir -p $INSTALL_DIR
+mkdir -p $INSTALL_DIR
 
 # Download Splunk
-sudo curl -O $SPLUNK_URL
+curl -O $SPLUNK_URL
 
 # Extract Splunk
-sudo tar -xzf splunk-*.tgz -C $INSTALL_DIR --strip-components=1
+tar -xzf splunk-*.tgz -C $INSTALL_DIR --strip-components=1
 
 # Start Splunk with username and password
-sudo $INSTALL_DIR/bin/splunk start --accept-license --answer-yes 
+$INSTALL_DIR/bin/splunk start --accept-license --answer-yes 
 
 #-auth $SPLUNK_USERNAME:$SPLUNK_PASSWORD
 
