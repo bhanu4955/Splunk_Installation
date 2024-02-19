08bbf0ae4383b8d82f000b767e8a1d20f71704e9
bhanu4955 <65145092+bhanu4955@users.noreply.github.com>
2024-02-19 20:34:21 +0530

    Update install_splunk.sh

--- install_splunk.sh
+++ install_splunk.sh
@@ -2,8 +2,8 @@
 
 SPLUNK_URL="https://download.splunk.com/products/splunk/releases/9.2.0.1/linux/splunk-9.2.0.1-d8ae995bf219-Linux-x86_64.tgz"
 INSTALL_DIR="/opt/splunk"
-SPLUNK_USERNAME="admin"
-SPLUNK_PASSWORD="bhargavi23"
+#SPLUNK_USERNAME="admin"
+#SPLUNK_PASSWORD="bhargavi23"
 
 # Create the installation directory
 sudo mkdir -p $INSTALL_DIR
@@ -15,6 +15,8 @@ sudo curl -O $SPLUNK_URL
 sudo tar -xzf splunk-*.tgz -C $INSTALL_DIR --strip-components=1
 
 # Start Splunk with username and password
-sudo $INSTALL_DIR/bin/splunk start --accept-license --answer-yes -auth $SPLUNK_USERNAME:$SPLUNK_PASSWORD
+sudo $INSTALL_DIR/bin/splunk start --accept-license --answer-yes 
+
+#-auth $SPLUNK_USERNAME:$SPLUNK_PASSWORD
 
 
