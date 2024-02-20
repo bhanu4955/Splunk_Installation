9b636fbaac4af8747c554bb557c3c4af75980a4b
bhanu4955 <65145092+bhanu4955@users.noreply.github.com>
2024-02-20 21:30:04 +0530

    Update install_splunk.sh

--- install_splunk.sh
+++ install_splunk.sh
@@ -4,14 +4,11 @@ SPLUNK_FILENAME="splunk-9.2.0.1-d8ae995bf219-Linux-x86_64.tgz"
 SPLUNK_URL="https://download.splunk.com/products/splunk/releases/9.2.0.1/linux/${SPLUNK_FILENAME}"
 INSTALL_DIR="/opt/splunk"
 
-# Create the installation directory
-sudo mkdir -p $INSTALL_DIR
-
 # Download Splunk
-sudo wget -O $SPLUNK_URL
+sudo wget -O ${SPLUNK_FILENAME} "$SPLUNK_URL"
 
 # Extract Splunk
-sudo tar -xzf splunk-*.tgz -C $INSTALL_DIR --strip-components=1
+sudo tar -xzf ${SPLUNK_FILENAME} -C $INSTALL_DIR --strip-components=1
 
 #permissions
 sudo chmod +x ${SPLUNK_FILENAME}
